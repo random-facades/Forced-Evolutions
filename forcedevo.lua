bbd34d2e485f169513444d86fed8157d593d6a6b
random-facades <random.facades@gmail.com>
2025-02-19 21:45:17 -0500

    Update forcedevo.lua

--- forcedevo.lua
+++ forcedevo.lua
@@ -2,6 +2,6 @@
 
 forcedevo_saved_func = item_evo
 item_evo = function(self, card, context, forced_key)
-   local new_context = {end_of_round = true}
+   local new_context = {end_of_round = true, blueprint = context.blueprint}
    return forcedevo_saved_func(self,card,new_context, forced_key)
 end
