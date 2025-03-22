d547218ed8129a1219d54a8d35e72fb8358d8d5a
random-facades <random.facades@gmail.com>
2025-03-22 08:32:27 -0400

    Prevent duplicate evolutions

--- forcedevo.lua
+++ forcedevo.lua
@@ -1,7 +1,10 @@
-
+-- Simple force the evolution
 
 forcedevo_saved_func = item_evo
 item_evo = function(self, card, context, forced_key)
-   local new_context = {end_of_round = true, blueprint = context.blueprint}
-   return forcedevo_saved_func(self,card,new_context, forced_key)
+   if card.is_evolving or context.end_of_round then return end
+   local ret = forcedevo_saved_func(self, card, { end_of_round = true }, forced_key)
+   if ret and ret.message then
+      card.is_evolving = true
+   end
 end
