e7c6b4714268edd8994507a727b40a20bf1fca18
random-facades <random.facades@gmail.com>
2025-03-22 08:49:55 -0400

    Fix evolution message not triggering

--- forcedevo.lua
+++ forcedevo.lua
@@ -2,9 +2,10 @@
 
 forcedevo_saved_func = item_evo
 item_evo = function(self, card, context, forced_key)
-   if card.is_evolving or context.end_of_round then return end
+   if card.is_evolving then return end
    local ret = forcedevo_saved_func(self, card, { end_of_round = true }, forced_key)
    if ret and ret.message then
       card.is_evolving = true
    end
+   return ret
 end
